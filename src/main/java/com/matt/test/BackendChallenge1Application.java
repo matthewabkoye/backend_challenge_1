a3c32b697e7cbb27e3dab97a6103178815115338
matthew.abikoye <matthew.abikoye@etranzact.com>
2022-10-01 15:29:23 +0100

    Initial Commit

---
+++ src/main/java/com/matt/test/BackendChallenge1Application.java
@@ -0,0 +1,13 @@
+package com.matt.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChallenge1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChallenge1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/matt/test/BackendChallenge1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matt.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChallenge1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
